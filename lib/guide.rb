b3dde82cedf4a18b68b8adfca5fbfaa85015ab8d
Charles De Barros <charlesdebarros@yahoo.co.uk>
2015-09-02 21:56:02 +0100

    Adding formatting to the 'list' feature resulting table.

--- lib/guide.rb
+++ lib/guide.rb
@@ -57,16 +57,13 @@ class Guide
   end
 
   def list
-    puts "\nListing restaurants.\n\n"
+    output_action_header("Listing restaurants")
     restaurants = Restaurant.saved_restaurants
-    restaurants.each do |rest|
-      puts rest.name + " | " + rest.cuisine + " | " + rest.formatted_price
-    end
+    output_restaurant_table(restaurants)
   end
 
   def add
-    puts "\nAdd a restaurant\n\n".upcase
-    
+    output_action_header("Adding restaurants")
     restaurant = Restaurant.profile_builder
 
     if restaurant.save
@@ -83,6 +80,27 @@ class Guide
 
   def conclusion
     puts "\n<<< Goodbye and enjoy your meal! >>>\n\n\n"
+  end
+
+  private
+
+  def output_action_header(text)
+    puts "\n#{text.upcase.center(60)}\n\n"
+  end
+
+  def output_restaurant_table(restaurants=[])
+    print " " + "Name".ljust(30)
+    print " " + "Cuisine".ljust(20)
+    print " " + "Price".rjust(6) + "\n"
+    puts "-" * 60
+    restaurants.each do |rest|
+      line = " " << rest.name.ljust(30)
+      line << " " + rest.cuisine.ljust(20)
+      line << " " + rest.formatted_price.rjust(6)
+      puts line
+    end
+    puts "No listings found." if restaurants.empty?
+    puts "-" * 60
   end
 
 end
