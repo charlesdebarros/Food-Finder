ec8e99b1cabc2d45306c412cb2f50f873ea1b935
Charles De Barros <charlesdebarros@yahoo.co.uk>
2015-09-02 17:19:39 +0100

    Setting up application path using $: and .unshift method.

---
+++ init.rb
@@ -0,0 +1,10 @@
+##### FOOD FINDER #####
+#
+# Please launch this application from the command line
+# to get started
+# 
+
+APP_ROOT = File.dirname(__FILE__)
+
+$:.unshift(File.join(APP_ROOT, 'lib'))
+require 'guide'
