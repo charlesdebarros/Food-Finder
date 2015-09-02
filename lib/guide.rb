01667edfe30a4c36d1b1c24bc492ba4b04a64fd1
Charles De Barros <charlesdebarros@yahoo.co.uk>
2015-09-02 17:26:49 +0100

    Adding content to guide.rb - predictive tasks for guide.rb to run

---
+++ lib/guide.rb
@@ -0,0 +1,18 @@
+class Guide
+
+  def initialize(path=nil)
+    # locate the restaurant text file at path
+    # or create a new file
+    # exit if create fails
+  end
+
+  def launch!
+    # introductory text
+    # action loop
+    #   what would you like to do? (list, find, add, quit)
+    #   do that action
+    # repeat until user chooses to quit
+    # conclusion text
+  end
+
+end
